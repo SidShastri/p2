604459db2a716a7ff573cbb3dba05dced76c5538
SidShastri <109056979+SidShastri@users.noreply.github.com>
2022-07-21 16:10:27 -0700

    nothing

---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>home</title>
+      <link rel="stylesheet" href="p2.css">
+
+  </head>
+
+<body>
+
+
+<div class="parallax1"></div>
+
+<div class='haha'>
+Are you ready to begin your journey?
+
+<a href='signin.php'>log in </a>
+<a href='signup.php'>sign up </a>
+</div>
+
+<div class="parallax2"></div>
+
+</body>
+
+</html>
